42d469dd7a1857c6e507176919a7cda58af36b28
mark-oblack <moblack42@gmail.com>
2017-12-20 18:11:32 -0500

    added Ron Swanson API and configured JS to generate new quote

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,15 @@
 
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$(".getQuote").click(function() {
 		$(".quote").html("New Quote");
 		$(".author").html("New Author");
+	});
+});*/
+
+$(".getQuote").click(function() {
+	$.getJSON("http://ron-swanson-quotes.herokuapp.com/v2/quotes", function(json) {
+		$.each(json, function() {
+			$(".quote").html('"' + this + '"');
+		});
 	});
 });
